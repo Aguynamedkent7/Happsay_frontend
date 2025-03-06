db4b695870b7aaf28061ecaab4c6fc1adc24c3a8
seodowa <pagalan.theodoreustp@gmail.com>
2025-03-06 22:16:39 +0800

    added comments to response interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -24,18 +24,27 @@ api.interceptors.response.use(
     return response;
     },
   async (error) => {
+    // Handle 401 Unauthorized error
     const originalRequest = error.config;
+
+    // If the request is unauthorized, try to refresh the access token
     if (error.response && error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       const refreshToken = localStorage.getItem('refresh_token');
+
+      // If refresh token is available, send a request to refresh the access token
       if (refreshToken) {
         try {
           const response = await axios.post(`https://happsay-backend-dev.ap-southeast-1.elasticbeanstalk.com/token/refresh/`, {
             refresh: refreshToken,
           });
+
           localStorage.setItem('access_token', response.data.access);
+
+          // Update the Authorization header and retry the original request
           api.defaults.headers['Authorization'] = `Bearer ${response.data.access}`;
           originalRequest.headers['Authorization'] = `Bearer ${response.data.access}`;
+          
           return api(originalRequest);
         } catch (err) {
           console.error('Failed to refresh token', err);
