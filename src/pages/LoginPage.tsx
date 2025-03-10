37c07b290c31162c50d84220c55cc073e3fc2411
Aguynamedkent7 <butaya.kentvincent07@gmail.com>
2025-03-10 23:40:07 +0800

    container bug

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "@/styles/LoginPage.css";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AxiosError } from "axios";
 import { Eye, EyeOff } from "lucide-react";
