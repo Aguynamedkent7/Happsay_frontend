3a057c8f4e58fb1a238cec6d7162e01e4c7d9e65
seodowa <pagalan.theodoreustp@gmail.com>
2025-03-18 01:43:08 +0800

    yes

--- src/components/types/types.ts
+++
@@ -1,2 +0,0 @@
-export type Todo = { id: number; title: string; content: string; is_done: boolean; is_archived: boolean; deadline: string };
-export type NotesState = { ToDo: Todo[]; Done: Todo[]; Archive: Todo[] };
